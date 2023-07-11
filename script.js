f6279533be1a465042ff095a3e65816deb95ab91
Erin.Schneider <erinsuzy@gmail.com>
2023-07-10 20:10:58 -0500

    planets and preventDefault

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 // Write your JavaScript code here!
 
+const { myFetch, pickPlanet, addDestinationInfo } = require("./scriptHelper");
+
 window.addEventListener("load", function() {
 
     let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
+    let listedPlanetsResponse = myFetch();
     listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
         //Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+   let planetSelect = pickPlanet(listedPlanets);
+   addDestinationInfo(document,planetSelect.name, planetSelect.diameter, planetSelect.star, planetSelect.distance, planetSelect.moons, planetSelect.image);
+    })
    
+});
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    let pilotInput = document.querySelector("input[name=pilotName]");
+    let pilot = pilotInput.value;
+
+    let copilotInput = document.querySelector("input[name=copilotName]");
+    let copilot = copilotInput.value;
+
+    let fuelInput = document.querySelector("input[name=fuelLevel]");
+    let fuelLevel = Number(fuelInput.value);
+
+    let cargoInput = document.querySelector("input[name=cargoMass]");
+    let cargoLevel = Number(cargoInput.value);
+
+    formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -84,6 +84,7 @@ function pickPlanet(planets) {
     return planets[PlanetX];
 }
 
+
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
